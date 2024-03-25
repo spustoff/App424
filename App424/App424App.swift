883bacf099915c82dde8d174d8f047ebb93e6a36
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-03-25 21:23:31 +0300

    Initial Commit

---
+++ App424/App424App.swift
@@ -0,0 +1,17 @@
+//
+//  App424App.swift
+//  App424
+//
+//  Created by IGOR on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct App424App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App424/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App424
+//
+//  Created by IGOR on 25/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
